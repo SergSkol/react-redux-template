382d3c83189d41371cbdf6eb627e1235470b954b
Serg Skol <sps676@gmail.com>
2023-06-01 10:23:31 +0200

    Adjust App

--- src/App.js
+++ src/App.js
@@ -6,13 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Counter />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
       </header>
     </div>
   );
